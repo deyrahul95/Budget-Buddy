bd59c04f80ccb334fff0e64423065cb11123de10
Rahul Dey <rahuldey9623@gmail.com>
2025-12-17 14:12:49 +0000

    Added Sqlite setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
+import { DBConfig } from "@/config/dbConfig";
 import { Stack } from "expo-router";
+import { SQLiteProvider } from "expo-sqlite";
+import { Suspense } from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Suspense>
+      <SQLiteProvider
+        databaseName={DBConfig.DBName}
+        assetSource={{ assetId: DBConfig.DBAssetPath }}
+        useSuspense
+      >
+        <Stack>
+          <Stack.Screen
+            name="home"
+            options={{
+              headerTitle: "Budget Buddy",
+              headerTitleAlign: "center",
+              headerLargeTitleEnabled: true,
+            }}
+          />
+        </Stack>
+      </SQLiteProvider>
+    </Suspense>
+  );
 }
---
+++ config/dbConfig.ts
@@ -0,0 +1,4 @@
+export const DBConfig = {
+    DBName: "budget.db",
+    DBAssetPath: require("@/assets/db/budget.db"),
+}
